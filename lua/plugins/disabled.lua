4578e12689d65b519972cf1787cc9892576ff3ed
javonteo <javonteo@hotmail.com>
2026-03-02 18:41:57 +0800

    Play alpha header

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,13 +1,16 @@
 return {
-	-- disable in favour of explorer.lua
-	{ "folke/snacks.nvim", 
-		opts = {
-			explorer = { enabled = false },
-		},
-		keys = {
-			{ "<leader>fe", false},
-			{ "<leader>fE", false},
-		},
-	},
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false }
+  -- disable in favour of explorer.lua
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+    keys = {
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+    },
+  },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- disable to remove bufferline (top buffers)
+  { "akinsho/bufferline.nvim", enabled = false },
 }
